80203a953a569e0d614400a2c9b4c1a6a7f0e3f7
ArsalanShakil <arsalan_cr7@yahoo.com>
2021-08-26 15:21:23 +0300

    version 1.1

--- app/src/main/java/com/example/w1d3_networkandthreadslab/MainActivity.kt
+++ app/src/main/java/com/example/w1d3_networkandthreadslab/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.example.w1d3_networkandthreadslab
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.os.Message
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.InputStream
+import java.net.HttpURLConnection
+import java.net.URL
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (isConnected) showData.text = "yes"
+        else
+            showData.text = "no"
+
     }
+
+    val Context.isConnected: Boolean
+        get() {
+            return (getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager)
+                .activeNetworkInfo?.isConnected == true
+        }
 }
+
+
